3b1d5bda143deca3bbb177c05735525abca2405f
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-17 19:22:17 +0600

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,29 @@ function App() {
   return (
     <> 
       <h1>Vite + React</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Student></Student>
     </>
+  )
+}
+
+function Person() {
+  const age = 25;
+  const money = 20;
+  const person = {name: 'sakib', age: 12}
+  return <h3>I am {person.name} with age: {age + money}</h3>
+}
+
+function Student() {
+  return (
+  <div>
+    <h3>This is a Student</h3>
+    <p>Name: </p>
+    <p>age: </p>
+  </div>
   )
 }
 
