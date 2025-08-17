9b7bb1e533a6ab119c9cc87e15ba6e8304e67e8c
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-17 19:56:31 +0600

    jsx and props

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,21 @@ function App() {
   return (
     <> 
       <h1>Vite + React</h1>
+      <Device name="laptop" price="55000"></Device>
+      <Device name="mobile" price="17000"></Device>
+      <Device name="watch" price="3000"></Device>
       <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Student grade="7" score="99"></Student>
+      <Student grade={12} score="85"></Student>
       <Student></Student>
+      <Developer></Developer>
     </>
   )
+}
+
+function Device(props) {
+  // console.log(props)
+  return <h2>This Device: {props.name} price: {props.price}</h2>
 }
 
 function Person() {
@@ -23,13 +31,31 @@ function Person() {
   return <h3>I am {person.name} with age: {age + money}</h3>
 }
 
-function Student() {
+const {grade, score} = {grade: '7',score: '99'};
+
+function Student({grade=1, score=0}) {
+  console.log(grade, score)
   return (
-  <div>
+  <div className='student'>
     <h3>This is a Student</h3>
-    <p>Name: </p>
-    <p>age: </p>
+    <p>Class: {grade}</p>
+    <p>Score: {score}</p>
   </div>
+  )
+}
+
+function Developer() {
+  const developerStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '10px'
+  }
+  return (
+    <div style={developerStyle}>
+      <h5>devo devo</h5>
+      <p>Coding</p>
+    </div>
   )
 }
 
